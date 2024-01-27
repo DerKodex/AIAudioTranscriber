4e1fa2ecde5bd2c305ef43fd1d74bbaad4bdd3d0
Víctor Daniel Aular <13076064+vdaular-dev@users.noreply.github.com>
2024-01-27 10:27:23 -0300

    Update 02_📝_Feedback.py

--- pages/02_📝_Feedback.py
+++ pages/02_📝_Feedback.py
@@ -11,8 +11,8 @@ st.set_page_config(
         layout= "wide",
         initial_sidebar_state="expanded",
         menu_items={
-        'Get Help': 'https://github.com/smaranjitghose/AIAudioTranscriber',
-        'Report a bug': "https://github.com/smaranjitghose/AIAudioTranscriber/issues",
+        'Get Help': 'https://github.com/DerKodex/AIAudioTranscriber',
+        'Report a bug': "https://github.com/DerKodex/AIAudioTranscriber/issues",
         'About': "## A minimalistic application to generate transcriptions for audio built using Python"
         } )
 
@@ -35,16 +35,5 @@ st_lottie(anim,
             width=400,
             key=None,
             )
-# HTML code for formsubmit contactform template
-contact_form = """
-            <form action="https://formsubmit.co/b2365a7af4d69269a92869bf9be52f1ba" method="POST">
-            <input type="hidden" name="_captcha" value="true">
-            <input type="hidden" name="_template" value="table">
-            <input type="text" name="name" placeholder="Your name" required>
-            <input type="email" name="email" placeholder="Your email" required>
-            <textarea name="message" placeholder="Your message here"></textarea>
-            <button type="submit">Send</button>
-            </form>
-            """
-st.markdown(contact_form,unsafe_allow_html=True)
+
 
